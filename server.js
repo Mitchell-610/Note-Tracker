753e2d4d2a932f0cae2fb2b5ffa1bc99cc85bbd0
Mitchell-610 <Mheit602@gmail.com>
2024-08-05 12:00:05 -0400

    Middlewear used and express called.

--- server.js
+++ server.js
@@ -7,3 +7,6 @@ const htmlRoutes = require('./public/routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
