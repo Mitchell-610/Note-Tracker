7e7a901e44ed5a7bb19a919ea4e43169dc9b12a8
Mitchell-610 <Mheit602@gmail.com>
2024-08-05 12:03:32 -0400

    We call get to set the landing page for url direction.

--- public/routes/htmlRoutes.js
+++ public/routes/htmlRoutes.js
@@ -3,3 +3,9 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+// Serve the landing page
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+// Serve the notes page
