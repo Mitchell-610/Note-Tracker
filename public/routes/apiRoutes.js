6e4e3225e9214959614c2e7c438d39248ca9bb74
Mitchell-610 <Mheit602@gmail.com>
2024-08-05 12:07:28 -0400

    We create a path to our database.

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -4,3 +4,5 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
+// Define the file path for storing notes
+const notesFilePath = path.join(__dirname, '..', '..', 'db', 'db.json');
